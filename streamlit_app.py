e29612c7b1f429c18cfdb71d9a7bbf5b884635b4
VictoriiaNestrugina <53817020+VictoriiaNestrugina@users.noreply.github.com>
2022-11-24 18:20:45 +0300

    fix try 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+fruit_choice = streamlit.text_input('What fruit would you like to add to the list?')
 if fruit_choice != '':
-  my_cur.execute(f"INSERT INTO fruit_load_list VALUES ({fruit_choice})")
+  my_cur.execute(f"INSERT INTO fruit_load_list (fruit_name) VALUES ({fruit_choice})")
