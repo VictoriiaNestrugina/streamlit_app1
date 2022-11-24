248bd9925e09b3f07a32e10da6f8d6e4af51aec5
VictoriiaNestrugina <53817020+VictoriiaNestrugina@users.noreply.github.com>
2022-11-24 14:36:33 +0300

    fix try 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if fruit_choice != '':
-  my_cur.execute(f"INSERT INTO fruit_load_list VALUES {fruit_choice}")
+  my_cur.execute(f"INSERT INTO fruit_load_list VALUES ({fruit_choice})")
