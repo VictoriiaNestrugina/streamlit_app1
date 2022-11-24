446987c636eba57a25b785e07a3af7c403d5eaee
VictoriiaNestrugina <53817020+VictoriiaNestrugina@users.noreply.github.com>
2022-11-24 14:34:40 +0300

    fix empty line error

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
-my_cur.execute(f"INSERT INTO fruit_load_list VALUES {fruit_choice}")
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if fruit_choice != '':
+  my_cur.execute(f"INSERT INTO fruit_load_list VALUES {fruit_choice}")
